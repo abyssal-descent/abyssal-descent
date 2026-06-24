4c6c35b399474b49d705374c51abb473ffb35308
slbsh <crafter.antek@gmail.com>
2026-06-25 00:11:57 +0200

    feat: stonecutter deals damage VERY IMPORTANT

--- src/adresources/src/main/java/AbyssalDescent/ADResources.java
+++ src/adresources/src/main/java/AbyssalDescent/ADResources.java
@@ -2,11 +2,15 @@ package AbyssalDescent.adresources;
 
 import AbyssalDescent.adresources.Registry;
 
+import net.minecraft.world.level.block.Blocks;
+
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingException;
 import net.minecraftforge.fml.ModLoadingStage;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.Arrays;
 
@@ -31,5 +35,19 @@ public class ADResources {
 					("\n   Abyssal Descent is INCOMPATIBLE with " + id).repeat(50),
 					new IllegalStateException("incompatible mod"));
 			});
+	}
+
+	@Mod.EventBusSubscriber(modid = MODID)
+	public static class Events {
+		@SubscribeEvent
+		public static void onPlayerTick(TickEvent.PlayerTickEvent e) {
+			if (e.phase != TickEvent.Phase.END) return;
+			
+			var level = e.player.level();
+			if (level.isClientSide()) return;
+			if (!level.getBlockState(e.player.blockPosition()).is(Blocks.STONECUTTER)) return;
+
+			e.player.hurt(e.player.damageSources().generic(), 1.0F);
+		}
 	}
 }
